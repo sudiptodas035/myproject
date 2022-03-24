6d66295da9539a75cca8d7c63e32ad313dc6b3c6
sudiptodas035 <sudiptadas025@gmail.com>
2022-03-25 02:17:41 +0530

    Added comments and Ststus method in TC_SearchCustomer

--- src/test/java/com/sdet/TC_SearchCustomer.java
+++ src/test/java/com/sdet/TC_SearchCustomer.java
@@ -4,4 +4,9 @@ public class TC_SearchCustomer {
 //This is my search customer test...
 	//This is my search customer test...
 	//This is my search customer test...
+	public void Status()
+	{
+		
+	System.out.println("Printing...");
+	}
 }
